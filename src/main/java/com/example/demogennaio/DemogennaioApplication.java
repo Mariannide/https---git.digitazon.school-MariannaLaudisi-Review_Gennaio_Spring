a4421136ea270366dbc3290d0982538e342713b7
Marianna <laudisimarianna@gmail.com>
2023-01-10 15:23:08 +0100

    Spring

---
+++ src/main/java/com/example/demogennaio/DemogennaioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogennaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogennaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogennaioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demogennaio/DemogennaioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogennaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogennaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
